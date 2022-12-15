c8a80bc732fdc440779b1f38e18471a6659b6c45
Medvedev Evgeny <medvedevea@elfin.tech>
2022-12-15 16:08:47 +0300

    initial commit

--- src/parser.test.ts
+++
@@ -1,9 +0,0 @@
-import { parser } from "./parser";
-
-describe("operations are getting parsed correctly", () => {
-  it("numbers and operators are getting parsed correctly", () => {
-    expect(parser("2 + 2")).toEqual([2, "+", 2]);
-    expect(parser("2 * 2")).toEqual([2, "*", 2]);
-    expect(parser("2 * 2 - 3 / 3")).toEqual([2, "*", 2, "-", 3, "/", 3]);
-  });
-});
